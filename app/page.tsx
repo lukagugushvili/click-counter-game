9ac02c3a2b3749d7b156294c820df045f447d00e
lukagugushvili <gugushvili58@gmail.com>
2024-04-28 03:00:48 -0700

    styled all components and add restart game function

--- app/page.tsx
+++ app/page.tsx
@@ -7,38 +7,51 @@ export default function Home() {
   const [increase, setIncrease] = useState<number>(0);
   const [endGame, setEndGame] = useState<boolean>(false);
 
-  // useEffect(() => {
-  //   setTimeout(() => {
-  //     if (timer > 0) {
-  //       setTime(timer - 1);
-  //     } else {
-  //       setEndGame(true);
-  //     }
-  //   }, 1000);
-  // });
+  useEffect(() => {
+    setTimeout(() => {
+      if (timer > 0) {
+        setTime(timer - 1);
+      } else {
+        setEndGame(true);
+      }
+    }, 1000);
+  });
 
   const handleIncreaseClick = () => {
     setIncrease(increase + 1);
   };
 
+  const handleRestartGame = () => {
+    setEndGame(false);
+    setTime(10);
+    setIncrease(0);
+  };
+
   return (
-    <main className="h-screen pt-10 flex flex-col justify-start items-center gap-6">
+    <main className="h-screen pt-10 flex flex-col justify-start items-center gap-7">
       {endGame ? (
         <>
-          <h1>your score is-{increase}</h1>
-          <button>Reset Game</button>
+          <h1 className="text-6xl text-sky-800">your score is-{increase}</h1>
+          <button
+            className="btn btn-outline btn-accent"
+            onClick={handleRestartGame}
+          >
+            Restart Game?
+          </button>
         </>
       ) : (
         <>
-          <h1>Click Counter Game</h1>
-          <p className="flex items-center text-6xl text-sky-600">
+          <h1 className="text-6xl text-sky-800">Click Counter Game</h1>
+          <p className="flex items-center text-5xl text-sky-700">
             timer -{" "}
             <span className="countdown font-mono text-6x">
               <span style={{ "--value": timer } as React.CSSProperties}></span>
             </span>
           </p>
-          <h3>score - {increase}</h3>
-          <button onClick={handleIncreaseClick}>Click it</button>
+          <h3 className="text-4xl text-sky-600">score - {increase}</h3>
+          <button className="btn btn-info" onClick={handleIncreaseClick}>
+            Click it
+          </button>
         </>
       )}
     </main>
